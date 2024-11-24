0e6cad26e03b1b31eb0e129dc4f1f45ca6dca4b4
최은영 <dev.choiey@gmail.com>
2024-11-24 16:40:04 +0900

    UPDATE: GloabalExceptionHandler에 @Valid 유효성 검사 예외 처리할 핸들러 추가

--- first/src/main/java/org/myweb/first/common/exception/GlobalExceptionHandler.java
+++ first/src/main/java/org/myweb/first/common/exception/GlobalExceptionHandler.java
@@ -1,15 +1,54 @@
 package org.myweb.first.common.exception;
 
 import org.myweb.first.common.ApiResponse;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // 모든 예외들을 핸들
+    // @Valid 어노테이션을 통해 유효성 검사할 때 발생하는 예외
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String, String> errors=new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName= ((FieldError) error).getField(); // 필드 이름
+            String errorMessage=error.getDefaultMessage(); // 오류 메시지
+            errors.put(fieldName, errorMessage);
+        });
+
+        ApiResponse<Map<String, String>> response = ApiResponse.<Map<String, String>>builder()
+                .success(false)
+                .message("유효성 검사 오류입니다.")
+                .data(errors)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+
+    }
+
+
+    // 데이터 무결성 예외 처리
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiResponse<String>> handleDataIntegrityViolationException(DataIntegrityViolationException ex){
+        ApiResponse<String> response = ApiResponse.<String>builder()
+                .success(false)
+                .message("데이터 무결성 위반: " + ex.getMostSpecificCause().getMessage())
+                .data(null)
+                .build();
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(response); // 409 Conflict 반환
+
+    }
+    // 유효성 검사 예외 처리
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<String>> handleAllExceptions(Exception e){
         ApiResponse<String> response = ApiResponse.<String>builder()
--- first/src/main/java/org/myweb/first/member/model/dto/Member.java
+++ first/src/main/java/org/myweb/first/member/model/dto/Member.java
@@ -2,8 +2,7 @@ package org.myweb.first.member.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,64 +16,71 @@ import java.util.Set;
 
 
 /*
-* 회원 정보 DTO (Data Transfer Object)
-* 데이터 이동 및 비즈니스 로직 처리에 사용
-* */
+ * 회원 정보 DTO (Data Transfer Object)
+ * 데이터 이동 및 비즈니스 로직 처리에 사용
+ * */
 @Data  //@Getter, @Setter, @ToString, @Equals, @HashCode 오버라이딩 까지 자동 생성됨
 @AllArgsConstructor // 모든 필드를 인자로 받는 생성자 생성
 @NoArgsConstructor // 기본 생성자 생성
 @Builder // 빌더 패턴 지원
 public class Member {
-	@NotBlank(message = "userId는 필수 입력 항목입니다.")  // 빈 값일 수 없음 (유효성 검사)
-	private String userId;  // 사용자 ID, DB 컬럼: USERID, 타입: VARCHAR2(50 BYTE)
-	@NotBlank(message="비밀번호는 필수 입력 항목입니다.")
-	@Size(min=8, message="비밀번호는 최소 8자리 이상이어야 합니다.")
-	private String userPwd; // 사용자 비밀번호, DB 컬럼: USERPWD, 타입: VARCHAR2(100 BYTE)+
-	@NotBlank(message="이름은 필수 입력 항목입니다.")
-	private String userName; // 사용자 이름, DB 컬럼: USERNAME, 타입: VARCHAR2(20 BYTE)
-	@NotBlank(message="성별은 필수 입력 항목입니다.")
-	private String gender;  // 성별, DB 컬럼: GENDER, 타입: CHAR(1 BYTE)
-	@NotBlank(message="성별은 필수 입력 항목입니다.")
-	private int age;   // 나이, DB 컬럼: AGE, 타입: NUMBER(3,0)
-	@NotBlank(message="성별은 필수 입력 항목입니다.")
-	private String phone;  // 전화번호, DB 컬럼: PHONE, 타입: VARCHAR2(13 BYTE)
-	@NotBlank(message="성별은 필수 입력 항목입니다.")
-	private String email;   // 이메일, DB 컬럼: EMAIL, 타입: VARCHAR2(30 BYTE)
-
-	@JsonFormat(pattern="yyyy-MM-dd")  // JSON 직렬화 시 날짜 형식 지정
-	private java.sql.Date enrollDate;  // 가입일, DB 컬럼: ENROLL_DATE, 타입: DATE
-
-	@JsonFormat(pattern="yyyy-MM-dd")
-	private java.sql.Date lastModified;  // 최종 수정일, DB 컬럼: LASTMODIFIED, 타입: DATE
-
-	private String signType;  // 가입 방식, DB 컬럼: SIGNTYPE, 타입: VARCHAR2(10 BYTE)
-	private String adminYN;  // 관리자 여부, DB 컬럼: ADMIN_YN, 타입: CHAR(1 BYTE)
-	private String loginOk;  // 로그인 허용 여부, DB 컬럼: LOGIN_OK, 타입: CHAR(1 BYTE)
-	private String photoFileName;  // 프로필 사진 파일명, DB 컬럼: PHOTO_FILENAME, 타입: VARCHAR2(100 BYTE)
-	private String roles;  // 사용자 역할, DB 컬럼: ROLES, 타입: VARCHAR2
-
-
+    @NotBlank(message = "userId는 필수 입력 항목입니다.")  // 빈 값일 수 없음 (유효성 검사)
+    private String userId;  // 사용자 ID, DB 컬럼: USERID, 타입: VARCHAR2(50 BYTE)
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8자리 이상이어야 합니다.")
+    private String userPwd; // 사용자 비밀번호, DB 컬럼: USERPWD, 타입: VARCHAR2(100 BYTE)+
+    @NotBlank(message = "이름은 필수 입력 항목입니다.")
+    private String userName; // 사용자 이름, DB 컬럼: USERNAME, 타입: VARCHAR2(20 BYTE)
+    @NotBlank(message = "성별은 필수 입력 항목입니다.")
+    private String gender;  // 성별, DB 컬럼: GENDER, 타입: CHAR(1 BYTE)
+    // @NotBlank(message="나이는 필수 입력 항목입니다.")
 	/*
-	* DTO를 엔터티로 변환하는 메소드
-	* @return 회원 엔터티
-	*/
-	public MemberEntity toEntity() {
-		return MemberEntity.builder()
-				.userId(this.userId)
-				.userPwd(this.userPwd)
-				.userName(this.userName)
-				.gender(this.gender)
-				.age(this.age)
-				.phone(this.phone)
-				.email(this.email)
-				.enrollDate(this.enrollDate)
-				.lastModified(this.lastModified)
-				.signType(this.signType)
-				.adminYN(this.adminYN)
-				.loginOk(this.loginOk)
-				.photoFileName(this.photoFileName)
-				// roles 필드 추가
-				.roles(this.roles)
-				.build();
-	}
+	@NotBlank는 String 타입에만 유효하며 , 빈 문자열 또는 공백 문자열을 검증하기 위한 어노테이션이다.
+	따라서 숫자 타입의 필드에 적용하면 유효성 검사 시젬에 예외가 발생하게 된다
+	 */
+    @Min(value = 0, message = "나이는 0 이상이어야 합니다.")
+    @Max(value = 300, message = "나이는 150 이하이어야 합니다.")
+    private int age;   // 나이, DB 컬럼: AGE, 타입: NUMBER(3,0)
+    @NotBlank(message = "전화번호는 필수 입력 항목입니다.")
+    private String phone;  // 전화번호, DB 컬럼: PHONE, 타입: VARCHAR2(13 BYTE)
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+    @Email(message="유효한 이메일 형식이어야 합니다.") // 이메일 형식 검증 어노테이션 추가
+    private String email;   // 이메일, DB 컬럼: EMAIL, 타입: VARCHAR2(30 BYTE)
+
+    @JsonFormat(pattern = "yyyy-MM-dd")  // JSON 직렬화 시 날짜 형식 지정
+    private java.sql.Date enrollDate;  // 가입일, DB 컬럼: ENROLL_DATE, 타입: DATE
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private java.sql.Date lastModified;  // 최종 수정일, DB 컬럼: LASTMODIFIED, 타입: DATE
+
+    private String signType;  // 가입 방식, DB 컬럼: SIGNTYPE, 타입: VARCHAR2(10 BYTE)
+    private String adminYN;  // 관리자 여부, DB 컬럼: ADMIN_YN, 타입: CHAR(1 BYTE)
+    private String loginOk;  // 로그인 허용 여부, DB 컬럼: LOGIN_OK, 타입: CHAR(1 BYTE)
+    private String photoFileName;  // 프로필 사진 파일명, DB 컬럼: PHOTO_FILENAME, 타입: VARCHAR2(100 BYTE)
+    private String roles;  // 사용자 역할, DB 컬럼: ROLES, 타입: VARCHAR2
+
+
+    /*
+     * DTO를 엔터티로 변환하는 메소드
+     * @return 회원 엔터티
+     */
+    public MemberEntity toEntity() {
+        return MemberEntity.builder()
+                .userId(this.userId)
+                .userPwd(this.userPwd)
+                .userName(this.userName)
+                .gender(this.gender)
+                .age(this.age)
+                .phone(this.phone)
+                .email(this.email)
+                .enrollDate(this.enrollDate)
+                .lastModified(this.lastModified)
+                .signType(this.signType)
+                .adminYN(this.adminYN)
+                .loginOk(this.loginOk)
+                .photoFileName(this.photoFileName)
+                // roles 필드 추가
+                .roles(this.roles)
+                .build();
+    }
 }
